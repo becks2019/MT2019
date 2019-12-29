531792a9dea677a1b475a353ab5ee73029ae52bd
Beatrice <beatrice.ins@web.de>
2019-12-29 19:33:52 +0100

    Taschenlampe, Zaun, Gras

---
+++ barnBurning/Assets/Flashlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+  public Light flashlight;
+  public bool activateFlashlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flashlight.enabled = activateFlashlight;
+        if(Input.GetKeyDown("f")){
+          activateFlashlight = !activateFlashlight;
+        }
+    }
+}
