2012f3fe4b7e689fcbace7ef6e21d5b33e4b86fe
Elisabeth Becker <elisabeth.becker.268@outlook.de>
2020-01-03 16:29:06 +0100

    Grab,Animationen,Pepsi,Sträucher

---
+++ barnBurning/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+	private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		_animator = GetComponent<Animator>();
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+	{
+			_animator.SetBool("open", true);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
