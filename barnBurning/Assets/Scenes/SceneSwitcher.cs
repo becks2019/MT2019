b34d66ff7d469f058625ee14d5c4e952ee8a141d
leni1998 <sobieraylena@googlemail.com>
2020-01-23 18:35:59 +0100

    scenechange desk to jogging

---
+++ barnBurning/Assets/Scenes/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider collider)
+    {
+    Debug.Log("Hallo");
+    SceneManager.LoadScene(0);
+    }
+    
+}
