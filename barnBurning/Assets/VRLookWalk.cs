2e695605655df048c96c44a8ab69055a3ec3a4f9
Beatrice <beatrice.ins@web.de>
2020-03-17 17:41:43 +0100

    Vr look down

---
+++ barnBurning/Assets/VRLookWalk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform vrCamera;
+    public float toggleAngle = 30.0f;
+    public float speed = 3.0f;
+    public bool moveForward;
+    public  CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponentInParent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f) {
+          moveForward = true;
+        }
+        else {
+          moveForward = false;
+        }
+
+        if(moveForward){
+          Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+          cc.SimpleMove(forward * speed);
+        }
+    }
+}
