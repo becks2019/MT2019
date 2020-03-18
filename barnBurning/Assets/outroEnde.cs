d3486161a8ed29393b923c9f90721806e5f1db9f
Elisabeth Becker <elisabeth.becker.268@outlook.de>
2020-03-18 15:15:03 +0100

    Outro und Skripte

---
+++ barnBurning/Assets/outroEnde.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class outroEnde : MonoBehaviour
+{
+    //Dieses Skript beendet die Applikation, wenn das Outro abgelaufen ist
+	public VideoPlayer VideoPlayer;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        //Beim Ende des Videos wird die Funktion EndApplication aufgerufen
+		VideoPlayer.loopPointReached += EndApplication;
+
+	}
+
+    // die Applikation wird beendet
+    void EndApplication(VideoPlayer vp)
+    {
+		Application.Quit();
+	}
+}
---
+++ barnBurning/Assets/wechselOutro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class wechselOutro : MonoBehaviour
+{
+
+    //Dieses Skript wechselt die Szene, wenn der Collider am Grab betreten wird
+	void OnTriggerEnter(Collider other)
+	{
+
+        //Wechsel zu Szene 3, dem Outro
+		SceneManager.LoadScene(3);
+
+	}
+
+}
